3cceb05ef3a6a3b378dfc2315b600e691c843a34
davidalvarez93 <david.alfredo.alvarez.morales@uabc.edu.mx>
2018-07-16 11:19:39 -0700

    Menu principal

---
+++ Components/Screens/FirstScreen.js
@@ -0,0 +1,11 @@
+
+export default class FirstScreen extends React.Component{
+    static navigationOptions = {
+        tabBarlabel: "Screen 1",
+        drawerIcon: ()=>{
+            return(
+                name
+            )
+        }
+    }
+}
--- Components/Screens/Home.js
+++ Components/Screens/Home.js
@@ -1,30 +1,96 @@
 import React from 'react';
-import { View, Text, Button} from 'react-native';
-import { createStackNavigator, DrawerNavigator } from 'react-navigation';
-import Drawer from 'react-native-drawer'
+import { View, Text, Button, ScrollView, StatusBar, Image, StyleSheet} from 'react-native';
+import { createStackNavigator, createDrawerNavigator, TabRouter } from 'react-navigation';
 import GlobalStyles from '../../GlobalStyles';
 
-//import logoin from '../Screens/LogInOrSignUp'
-import LoginOrSignUp from '../Screens/LogInOrSignUp';
+class MyHomeScreen extends React.Component {
+    static navigationOptions = {
+      drawerLabel: 'Home',
+      drawerIcon: ({ tintColor }) => (
+        <Image
+          source={require('../Images/chat.png')}
+          style={[styles.icon, {tintColor: tintColor}]}
+        />
+      ),
+    };
+  
+    render() {
+      return (
+<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> HOME PAGE! </Text> 
+        <Button
+          onPress={() => this.props.navigation.navigate('Notifications')}
+          title="Go to notifications"
+        />
+</View>
+      );
+    }
+  }
+  
+  class MyNotificationsScreen extends React.Component {
+    static navigationOptions = {
+      drawerLabel: 'Notifications',
+      drawerIcon: ({ tintColor }) => (
+        <Image
+          source={require('../Images/notificaciones.png')}
+          style={[styles.icon, {tintColor: tintColor}]}
+        />
+      ),
+    };
+  
+    render() {
+      return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> HOME PAGE! </Text> 
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Go back home"
+        />
+         <Button
+          onPress={() => this.props.navigation.navigate('DrawerOpen')}
+          title="Drawer Open"
+        />
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    icon: {
+      width: 24,
+      height: 24,
+    },
+  });
+  
+const MiApp = createDrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+});
+  //export default createDrawerNavigator({
+    //Home: {
+      //screen: MyHomeScreen,
+    //},
+    //Notifications: {
+      //screen: MyNotificationsScreen,
+    //},
+  //});
+/*
+  {
+drawerPosition: 'right',
+initialRouteName: 'Home',
+drawerBackgroundColor: 'orange',
+drawerWidth: 300,
 
-
-
+  });*/
+  /*
 class Screenr extends React.Component {
-    closeControlPanel = () => {
-        this._drawer.close()
-    };
-    openControlPanel = () => {
-        this._drawer.open()
-    };
     render() {
         return (
             <View style={GlobalStyles.FirstScreen}>
-            <Drawer
-            ref={(ref) => this._drawer = ref}
-            content={<ControlPanel />}
-            >
-            <MainView />
-            </Drawer>
                 <Text>HOMESCREEN</Text>      
                 <Button
                    onPress={() => this.props.navigation.goBack()}
@@ -36,19 +102,22 @@ class Screenr extends React.Component {
         );
     }
 }
-
+*/
+/*
 const RootStack = createStackNavigator(
   {
-    Home: Screenr,
+    Menu: MyHomeScreen,
     //Reg: LoginOrSignUp,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Menu',
     headerMode: 'none',
   },
-);
+);*/
 
 export default class App extends React.Component {
   render() {
-    return <RootStack />;
+   // return <RootStack />;
+   return <MiApp/>;
   }}
+  
