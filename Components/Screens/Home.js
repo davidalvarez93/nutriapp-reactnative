0970a23314a58c1415ed4852d1e5030c96ecf7a4
Jorge-Bau <jorge.bautista@tectijuana.edu.mx>
2018-08-02 21:06:08 -0700

    Login y perfil

--- Components/Screens/Home.js
+++ Components/Screens/Home.js
@@ -1,124 +1,189 @@
 import React from 'react';
-import { View, Text, Button, ScrollView, StatusBar, Image, StyleSheet} from 'react-native';
+import { View, Text, Button, ScrollView, StatusBar, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, TabRouter } from 'react-navigation';
 import GlobalStyles from '../../GlobalStyles';
+import SignUp from '../Screens/SignUp';
 
 class MyHomeScreen extends React.Component {
-    static navigationOptions = {
-      drawerLabel: 'Home',
-      drawerIcon: ({ tintColor }) => (
-        <Image
-          source={require('../Images/chat.png')}
-          style={[styles.icon, {tintColor: tintColor}]}
-        />
-      ),
-    };
-  
-    render() {
-      return (
-<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> HOME PAGE! </Text> 
-        <Button
-          onPress={() => this.props.navigation.openDrawer()}
-          title="Drawer Open"
-        />
-</View>
-      );
-    }
-  }
-  
-class MyProfile extends React.Component{
   static navigationOptions = {
-    drawerLabel: 'Profile',
-    drawerIcon: ({tintColor}) => (
+    drawerLabel: 'Home',
+    drawerIcon: ({ tintColor }) => (
       <Image
-      source={require('../Images/profile.jpg')}
-      style={[styles.icon, {tintColor: tintColor}]}
+        source={require('../Images/chat.png')}
+        style={[styles.icon, { tintColor: tintColor }]}
       />
     ),
-};
+  };
 
-render(){
-  return(
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> HOME PAGE! </Text> 
+  render() {
+    <View style={styles.container}>
+      <Swiper
+        cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+        renderCard={(card) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.text}>{card}</Text>
+            </View>
+          )
+        }}
+        onSwiped={(cardIndex) => { console.log(cardIndex) }}
+        onSwipedAll={() => { console.log('onSwipedAll') }}
+        cardIndex={0}
+        backgroundColor={'#4FD0E9'}
+        stackSize={3}>
         <Button
-          onPress={() => this.props.navigation.goBack()}
-          title="Go back home"
-        />
-         <Button
-          onPress={() => this.props.navigation.closeDrawer()}
-          title="Drawer close"
-        />
-        </View>
-    );
-  } 
+          onPress={() => { console.log('oulala') }}
+          title="Press me">
+          You can press me
+            </Button>
+      </Swiper>
+    </View>
+  }
 }
 
-  class MyNotificationsScreen extends React.Component {
-    static navigationOptions = {
-      drawerLabel: 'Notifications',
-      drawerIcon: ({ tintColor }) => (
-        <Image
-          source={require('../Images/notificaciones.png')}
-          style={[styles.icon, {tintColor: tintColor}]}
-        />
-      ),
-    };
-  
-    render() {
-      return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> HOME PAGE! </Text> 
+
+
+class MyProfile extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Profile',
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require('../Images/profile.jpg')}
+        style={[styles.icon, { tintColor: tintColor }]}
+      />
+    ),
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> HOME PAGE! </Text>
         <Button
           onPress={() => this.props.navigation.goBack()}
           title="Go back home"
         />
-         <Button
-          onPress={() => this.props.navigation.navigate('DrawerOpen')}
-          title="Drawer Open"
-        />
-        </View>
-      );
-    }
+
+      </View>
+    );
   }
-  
-  const styles = StyleSheet.create({
-    icon: {
-      width: 24,
-      height: 24,
-    },
-  });
-  
+}
+
+
+
+class MyPaymentMethods extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Payment methods',
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require('../Images/notificaciones.png')}
+        style={[styles.icon, { tintColor: tintColor }]}
+      />
+    ),
+  };
+
+
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> This is the Payment methods screen </Text>
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Go back home"
+        />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
+
+class MyFlights extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'My Flights',
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require('../Images/notificaciones.png')}
+        style={[styles.icon, { tintColor: tintColor }]}
+      />
+    ),
+  };
+
+
+
+
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> This is the flights screen </Text>
+
+      </View>
+    );
+  }
+}
+
+
+class LogOutScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Log Out',
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require('../Images/notificaciones.png')}
+        style={[styles.icon, { tintColor: tintColor }]}
+      />
+    ),
+  };
+
+
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> Are you sure you want to log out? </Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('Sign')}
+          title="Log out"
+        />
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Go Back home"
+        />
+      </View>
+    );
+  }
+}
+
+
+
 const MiApp = createDrawerNavigator({
-  Home: {
-    screen: MyHomeScreen,
-  },
-  Notifications: {
-    screen: MyNotificationsScreen,
-  },
-  Profile: {
-    screen: MyProfile,
-  },
+  Home: { screen: MyHomeScreen, },
+  Profile: { screen: MyProfile, },
+  Payment: { screen: MyPaymentMethods, },
+  Flights: { screen: MyFlights, },
+  LogOut: { screen: LogOutScreen, },
+
 
 });
 
 export default createDrawerNavigator({
-    Home: {
-      screen: MyHomeScreen,
-    },
-    Notifications: {
-      screen: MyNotificationsScreen,
-    },
-    Profile: {
-      screen: MyProfile,
-    },
-  }, 
+  Home: { screen: MyHomeScreen, },
+  Profile: { screen: MyProfile, },
+  Payment: { screen: MyPaymentMethods, },
+  Flights: { screen: MyFlights, },
+  LogOut: { screen: LogOutScreen, },
+},
   {
-drawerPosition: 'right',
-initialRouteName: 'Home',
-drawerBackgroundColor: '#0000ff',
-drawerWidth: 300,
+    drawerPosition: 'left',
+    initialRouteName: 'Home',
+    drawerBackgroundColor: '#f0f8ff',
+    drawerWidth: 300,
   });
   /*
 class Screenr extends React.Component {
