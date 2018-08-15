12aaf36a95581e4ea3048e923eae2e645cf7f0be
scorbac <jose.lopez17@tectijuana.edu.mx>
2018-08-15 15:52:03 -0700

    restart

--- Components/Screens/DrawerNavigator.js
+++ Components/Screens/DrawerNavigator.js
@@ -2,13 +2,13 @@ import { createDrawerNavigator } from 'react-navigation';
 
 import HomeScreen from './Home'
 import Profile from './Profile'
-import Flights from './Flights'
+import BookFlights from './BookFlights'
 import Test from './Test2'
 import LogOut  from './LogOut'
 
 export default createDrawerNavigator({
   Home: { screen: HomeScreen },
-  Flights: { screen: Flights },
+  Flights: { screen: BookFlights },
   Test: { screen: Test },
   Profile: { screen: Profile },
   LogOut: { screen: LogOut },
